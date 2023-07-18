45672c934961df7c8ead457662ab08c30c6c90a7
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2023-07-18 17:38:03 +0300

    improve tests

--- test/main.test.ts
+++ test/main.test.ts
@@ -12,7 +12,6 @@ test('LinkSanitizer stack', () => {
 
   const messageHandlerId = 'Handler886CB40B';
   const messageSenderId = 'MessageSenderC8564C19';
-  const queueId = 'MessageQueue7A3BF959';
 
   // Check the snapshot
   expect(template.toJSON()).toMatchSnapshot();
@@ -21,6 +20,7 @@ test('LinkSanitizer stack', () => {
   const queues = template.findResources('AWS::SQS::Queue', {});
 
   expect(Object.keys(queues)).toHaveLength(1);
+  const queueId = Object.keys(queues)[0];
 
   expect(queues).toHaveProperty(queueId);
 
@@ -56,14 +56,12 @@ test('LinkSanitizer stack', () => {
     ],
   ).toEqual(tokenParameterName);
 
-  const eventSourceMappings = template.findResources('AWS::Lambda::EventSourceMapping', {
+  template.hasResource('AWS::Lambda::EventSourceMapping', {
     Properties: {
       EventSourceArn: { 'Fn::GetAtt': [queueId, 'Arn'] },
       FunctionName: { Ref: messageSenderId },
     },
   });
-  expect(Object.keys(eventSourceMappings)).toHaveLength(1);
-
 
   const messageHandlerRolePolicies = template.findResources('AWS::IAM::Policy', {
     Properties: {
@@ -117,4 +115,116 @@ test('LinkSanitizer stack', () => {
 
   expect(senderHasReadAccessToQueue).toBe(true);
 
+  const apis = template.findResources('AWS::ApiGateway::RestApi');
+  expect(Object.keys(apis)).toHaveLength(1);
+  const apiId = Object.keys(apis)[0];
+
+  const deploymentStages = template.findResources('AWS::ApiGateway::Stage', {
+    Properties: {
+      RestApiId: {
+        Ref: apiId,
+      },
+    },
+  });
+  expect(Object.keys(deploymentStages)).toHaveLength(1);
+
+  const apiProdStageId = Object.keys(deploymentStages)[0];
+
+  template.hasResource('AWS::ApiGateway::Method', {
+    Properties: {
+      Integration: {
+        IntegrationHttpMethod: 'POST',
+        Type: 'AWS_PROXY',
+        Uri: {
+          'Fn::Join': [
+            '',
+            [
+              'arn:',
+              {
+                Ref: 'AWS::Partition',
+              },
+              ':apigateway:',
+              {
+                Ref: 'AWS::Region',
+              },
+              ':lambda:path/2015-03-31/functions/',
+              {
+                'Fn::GetAtt': [
+                  messageHandlerId,
+                  'Arn',
+                ],
+              },
+              '/invocations',
+            ],
+          ],
+        },
+      },
+      RestApiId: {
+        Ref: apiId,
+      },
+    },
+  });
+
+  template.hasResource('Custom::TelegramBotWebhook', {
+    Properties: {
+      tokenParameterName: tokenParameterName,
+      webhookURL: {
+        'Fn::Join': [
+          '',
+          [
+            'https://',
+            {
+              Ref: apiId,
+            },
+            '.execute-api.',
+            {
+              Ref: 'AWS::Region',
+            },
+            '.',
+            {
+              Ref: 'AWS::URLSuffix',
+            },
+            '/',
+            {
+              Ref: apiProdStageId,
+            },
+            '/',
+          ],
+        ],
+      },
+    },
+  });
+
+
+  const senderHasReadAccessToToken = Object.values(messageSenderRolePolicies).some(
+    policy => {
+      return policy.Properties.PolicyDocument.Statement.some((statement: PolicyStatement) => {
+        const actions = Array.isArray(statement.Action) ? statement.Action : [statement.Action];
+        return actions.includes('ssm:GetParameter') &&
+          JSON.stringify(statement.Resource) === JSON.stringify(
+            {
+              'Fn::Join': [
+                '',
+                [
+                  'arn:',
+                  {
+                    Ref: 'AWS::Partition',
+                  },
+                  ':ssm:',
+                  {
+                    Ref: 'AWS::Region',
+                  },
+                  ':',
+                  {
+                    Ref: 'AWS::AccountId',
+                  },
+                  `:parameter/${tokenParameterName}`,
+                ],
+              ],
+            });
+      },
+      );
+    });
+
+  expect(senderHasReadAccessToToken).toBe(true);
 });
